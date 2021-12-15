559033f8966a76b700e30689a5becb9a0a098d8c
Trent <tjmulloy1@gmail.com>
2021-12-15 12:03:45 -0600

    week 1 dat 3 midpoint

---
+++ Web Fundementals_Dec2021/Algorithims/w1d2.js
@@ -0,0 +1,15 @@
+//problem 1
+var a = 25;
+a =  a - 13;
+console.log(a/2);
+
+a = "hello";
+console.log(a + "hello"); 
+
+//problem 2
+for(var i = 0; i<10; i++){
+    console.log(i);
+    i = i+3;
+}
+
+console.log("outside of the loop" + i); 
