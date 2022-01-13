212d516647c35c810702f19a433495a6cb4f5757
Trent <tjmulloy1@gmail.com>
2022-01-13 12:45:40 -0600

    w1d4 python Playground

--- Python Course/Flask/playground/server.py
+++ Python Course/Flask/playground/server.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template  # added render_template!
 app = Flask(__name__)                     
     
 
-@app.route('/')
-def index():
-    return render_template("index.html", phrase="hello", times=5)	# notice the 2 new named arguments!
+@app.route('/<int:x>/<color>')
+def index(x, color):
+    return render_template("index.html", phrase="", times=x, background=color )	
 
 
 
@@ -36,4 +36,3 @@ def index():
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
-
