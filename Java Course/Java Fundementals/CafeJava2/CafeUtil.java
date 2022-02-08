2cb03fa2b54b31604de70544b39cd072692556ac
Trent <tjmulloy1@gmail.com>
2022-02-07 18:55:07 -0600

    Cafe Business Logic.Java

--- Java Course/Java Fundementals/CafeJava2/CafeUtil.java
+++ Java Course/Java Fundementals/CafeJava2/CafeUtil.java
@@ -1,6 +1,8 @@
-import java.util.Date;
+// import java.util.Date;
+import java.util.ArrayList;
+// import java.util.Scanner;
 public class CafeUtil{
-    public String getStreakGoal()  {
+    public int getStreakGoal()  { 
 
         int num = 10, sum = 0;
 
@@ -8,8 +10,32 @@ public class CafeUtil{
         {
             sum += i;
         }
-        String x = String.valueOf(sum);
 
-        return x;
+        return sum;
+    }
+    public double getOrderTotal(double[] lineItems){
+            double sum = 0;
+            for(int i = 0; i < lineItems.length; i++)
+            {
+                sum = sum + lineItems[i];
+            }
+            return sum;
+        }
+    public String displayMenu(ArrayList<String> menu){
+        for(int i = 0; i < menu.size(); i++)
+            {
+            System.out.println(i + " " + menu.get(i));
+            }
+        return menu.get(1);
+    }
+    public void addCustomer(ArrayList<String> customers){
+        //Scanner inputText = new Scanner(System.in);
+        String userName = System.console().readLine();
+        // System.out.println("Please enter name");
+        // userName = inputText.nextLine();
+        System.out.printf("Hello, %s!", userName);
+        customers.add(userName);
+        System.out.println('\n' + "There are currently " + customers.size() + " customer(s) in line");
+        
     }
 }
